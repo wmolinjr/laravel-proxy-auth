ec732535b72267b46bc381379d2b1f9679c6dfd5
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2025-08-13 11:29:05 -0400

    Update forgot-password.tsx

--- resources/js/pages/auth/forgot-password.tsx
+++ resources/js/pages/auth/forgot-password.tsx
@@ -1,7 +1,6 @@
 // Components
-import { Head, useForm } from '@inertiajs/react';
+import { Form, Head } from '@inertiajs/react';
 import { LoaderCircle } from 'lucide-react';
-import { FormEventHandler } from 'react';
 
 import InputError from '@/components/input-error';
 import TextLink from '@/components/text-link';
@@ -11,16 +10,6 @@ import { Label } from '@/components/ui/label';
 import AuthLayout from '@/layouts/auth-layout';
 
 export default function ForgotPassword({ status }: { status?: string }) {
-    const { data, setData, post, processing, errors } = useForm<Required<{ email: string }>>({
-        email: '',
-    });
-
-    const submit: FormEventHandler = (e) => {
-        e.preventDefault();
-
-        post(route('password.email'));
-    };
-
     return (
         <AuthLayout title="Forgot password" description="Enter your email to receive a password reset link">
             <Head title="Forgot password" />
@@ -28,30 +17,25 @@ export default function ForgotPassword({ status }: { status?: string }) {
             {status && <div className="mb-4 text-center text-sm font-medium text-green-600">{status}</div>}
 
             <div className="space-y-6">
-                <form method="POST" onSubmit={submit}>
-                    <div className="grid gap-2">
-                        <Label htmlFor="email">Email address</Label>
-                        <Input
-                            id="email"
-                            type="email"
-                            name="email"
-                            autoComplete="off"
-                            value={data.email}
-                            autoFocus
-                            onChange={(e) => setData('email', e.target.value)}
-                            placeholder="email@example.com"
-                        />
+                <Form method="post" action={route('password.email')}>
+                    {({ processing, errors }) => (
+                        <>
+                            <div className="grid gap-2">
+                                <Label htmlFor="email">Email address</Label>
+                                <Input id="email" type="email" name="email" autoComplete="off" autoFocus placeholder="email@example.com" />
 
-                        <InputError message={errors.email} />
-                    </div>
+                                <InputError message={errors.email} />
+                            </div>
 
-                    <div className="my-6 flex items-center justify-start">
-                        <Button className="w-full" disabled={processing}>
-                            {processing && <LoaderCircle className="h-4 w-4 animate-spin" />}
-                            Email password reset link
-                        </Button>
-                    </div>
-                </form>
+                            <div className="my-6 flex items-center justify-start">
+                                <Button className="w-full" disabled={processing}>
+                                    {processing && <LoaderCircle className="h-4 w-4 animate-spin" />}
+                                    Email password reset link
+                                </Button>
+                            </div>
+                        </>
+                    )}
+                </Form>
 
                 <div className="space-x-1 text-center text-sm text-muted-foreground">
                     <span>Or, return to</span>
